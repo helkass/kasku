f9dc3f561b0915ca919b378739440fca2dab1da6
helkass <helkaseptyawan@gmail.com>
2025-11-30 23:49:15 +0700

    setting vercel.json and route SPA

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import MainLayout from "@/components/MainLayout.vue";
 import { useAuth } from "@/composables/useAuth";
 
@@ -83,7 +83,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
